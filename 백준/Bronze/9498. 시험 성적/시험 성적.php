64086136257258566a48c49ac4190628589d3ae3
Hyowon <132967458+hhyowon@users.noreply.github.com>
2024-01-24 14:54:14 +0900

    [Bronze V] Title: 시험 성적, Time: 16 ms, Memory: 83416 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.php
@@ -0,0 +1,19 @@
+<?PHP
+    
+    $score = (int)fgets(STDIN); 
+
+    if($score >= 90){
+        echo 'A';
+    } elseif ($score >= 80) {   
+    echo 'B';
+} elseif ($score >= 70) {   
+    echo 'C';
+} elseif ($score >= 60) {   
+    echo 'D';
+} else {                 
+    echo 'F';
+}
+    
+
+    
+?>
